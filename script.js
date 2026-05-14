c45f9e22ff8ee2a942eb2bc8018acf8ae003ee52
YinJonaR <3066821090601@ingenieria.usac.edu.gt>
2026-05-13 19:18:31 -0600

    Formulario Arreglado

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-const formOracion = document.getElementById("formOracion");
-const mensajeExito = document.getElementById("mensajeExito");
-
-formOracion.addEventListener("submit", function (e) {
-  e.preventDefault();
-
-  const nombre = document.getElementById("nombre").value.trim();
-  const motivo = document.getElementById("motivo").value.trim();
-
-  if (nombre === "" || motivo === "") {
-    mensajeExito.textContent = "Por favor completa todos los campos.";
-    mensajeExito.style.color = "red";
-    return;
-  }
-
-  mensajeExito.textContent = "Tu petición fue enviada correctamente. Estaremos orando por ti.";
-  mensajeExito.style.color = "green";
-
-  formOracion.reset();
-});
-
-
-
 
 const btnMensajes = document.getElementById("btnMensajes");
 
